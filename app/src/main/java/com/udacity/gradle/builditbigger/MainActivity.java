9f48fbfb7e3149f0571ba3fabfbca9d8f6b9c344
Gabriel Bernardo Pereira <gabeira@gmail.com>
2015-10-25 19:23:54 -0400

    feat: Include new Android Library module to display Jokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.udacity.android.JokeActivity;
 import com.udacity.java.Jokes;
 
 
@@ -43,7 +45,9 @@ public class MainActivity extends ActionBarActivity {
 
     public void tellJoke(View view) {
         Jokes jokes = new Jokes();
-        Toast.makeText(this, jokes.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokeActivity.class);
+        intent.putExtra(JokeActivity.JOKE_KEY, jokes.getJoke());
+        startActivity(intent);
     }
 
 

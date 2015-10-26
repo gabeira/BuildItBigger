78ff90131993fc89ef513bbeba34d8bf6e969aee
Gabriel Bernardo Pereira <gabeira@gmail.com>
2015-10-25 23:14:55 -0400

    feat: Include functional Tests for GCE AsyncTask

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/GoogleCloudEndpointsTest.java
@@ -0,0 +1,25 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+import android.util.Pair;
+
+/**
+ * Created by gabriel on 10/25/15.
+ */
+public class GoogleCloudEndpointsTest extends AndroidTestCase {
+
+    static final String TO_TEST = "testing joke";
+    static final String COMPARE_TO = "Hey, testing joke with Google Cloud Endpoints";
+
+    public void testJokeFromGCE() {
+
+        new EndpointsAsyncTask(new EndpointsAsyncTask.Callback() {
+            @Override
+            public void onLoadJokeFromGCE(String result) {
+                assertEquals(COMPARE_TO, result);
+            }
+        }).execute(new Pair<Context, String>(getContext(), TO_TEST));
+
+    }
+}
